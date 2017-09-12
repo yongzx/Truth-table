0780a3d09453738f08669a14af9630cc455509fa
yongzx <zhengxin.yong@minerva.kgi.edu>
2017-09-12 08:16:48 -0700

    Tree

--- logic.py
+++ logic.py
@@ -11,6 +11,10 @@ def split_into_list(s):
 	s = s.translate(remove_punc)
 	return s.split(" ")
 
+def remove_then(list_x):
+	list_x = [x for x in list_x if x != "if"]
+	return list_x
+
 class LogicPackage:
 	def __init__(self):
 		self.logic_package = []
@@ -42,7 +46,53 @@ class LogicPackage:
 	def show_package(self):
 		return self.logic_package
 
+class Node:
+	def __init__(self, data):
+		self.data = data
+		self.left = None
+		self.right = None
+		self.p = None
+
+class Tree:
+	def __init__(self):
+		self.root = None
+
+	def create_logic_tree(self, list_x):
+		self.root = Node(list_x)
+
+		def create_logic_tree_helper(node):
+			tmp = node.data
+			node.data = tmp[1]
+			node.left = Node(tmp[0])
+			node.right = Node(tmp[2])
+
+			if isinstance(node.left.data, list):
+				create_logic_tree_helper(node.left)
+			if isinstance(node.right.data, list):
+				create_logic_tree_helper(node.right)
+
+		create_logic_tree_helper(self.root)
+
+def inorder_traversal(tree):
+	if tree:
+		inorder_traversal(tree.left)
+		print(tree.data)
+		inorder_traversal(tree.right)
+
+def pre_order_traversal(tree):
+	if tree:
+		print(tree.data)
+		pre_order_traversal(tree.left)
+		pre_order_traversal(tree.right)
+
 s = split_into_list(s7)
+s = remove_then(s)
 L = LogicPackage()
 L.change_into_package(s)
-print(L.show_package())
+l = (L.show_package())
+print(l)
+T = Tree()
+T.create_logic_tree(l)
+inorder_traversal(T.root)
+print()
+pre_order_traversal(T.root)
